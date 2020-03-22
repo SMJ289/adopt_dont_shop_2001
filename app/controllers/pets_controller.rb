c66b4116e4bd8dab4419d5085d22f92fd79e12c9
Steven Meyers <sjjmeyers@gmail.com>
2020-03-22 08:08:03 -0600

    Add pet update functionality

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,6 +24,23 @@ class PetsController < ApplicationController
     redirect_to "/shelters/#{params[:id]}/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def update
+    pet = Pet.find(params[:pet_id])
+    pet.update({
+      name: params[:name],
+      image: params[:image],
+      description: params[:description],
+      approximate_age: params[:approximate_age],
+      sex: params[:sex],
+      })
+    pet.save
+    redirect_to "/pets/#{pet.id}"
+  end
+
   private
   def pet_params
     params.permit(:name, :image, :discription, :approximate_age, :sex)
